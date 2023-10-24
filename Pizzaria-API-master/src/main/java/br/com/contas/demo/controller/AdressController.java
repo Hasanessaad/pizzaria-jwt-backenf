2ca1acfbd4231e4719fb3aca7fe64ef40b199004
hasan essaad <hasanessaad@gmail.com>
2023-10-24 13:56:21 -0300

    findall for the adressController

--- Pizzaria-API-master/src/main/java/br/com/contas/demo/controller/AdressController.java
+++ Pizzaria-API-master/src/main/java/br/com/contas/demo/controller/AdressController.java
@@ -2,12 +2,15 @@ package br.com.contas.demo.controller;
 
 import br.com.contas.demo.dto.AdressDTO;
 import br.com.contas.demo.entity.Adress;
+import br.com.contas.demo.entity.Client;
 import br.com.contas.demo.service.AdressService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RestController
@@ -16,6 +19,12 @@ public class AdressController {
 
     @Autowired
     private AdressService service;
+
+    @GetMapping("/findall")
+
+    public List<Adress> Findall() {
+        return service.Findall();
+    }
 
     @PostMapping
     public ResponseEntity<Adress> create( @Valid @RequestBody AdressDTO adressDTO) {
--- Pizzaria-API-master/src/main/java/br/com/contas/demo/entity/Client.java
+++ Pizzaria-API-master/src/main/java/br/com/contas/demo/entity/Client.java
@@ -2,6 +2,7 @@ package br.com.contas.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -16,7 +17,6 @@ public class Client {
     private Long id;
 
     @Getter
-
     private String nome;
 
     @Getter
